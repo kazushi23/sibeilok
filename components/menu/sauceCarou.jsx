cad691b406ab7fa401e709f10d08fae0e258574f
kazushi23 <kazushit8@gmail.com>
2022-05-20 22:20:18 +0800

    fourth

--- components/menu/sauceCarou.jsx
+++ components/menu/sauceCarou.jsx
@@ -35,7 +35,7 @@ class SauceCarou extends Component {
     static get CONTAINER_STYLE() {
         return {
             position: "relative",
-            height: "40vh",
+            height: "50vh",
             width: "100%",
             display: "flex",
             flex: 1,
@@ -47,7 +47,7 @@ class SauceCarou extends Component {
 
     static get CARD_STYLE() {
         return {
-            height: "250px",
+            height: "300px",
             width: '65vw',
             maxWidth:"65vw",
             textAlign: 'center',
@@ -62,14 +62,14 @@ class SauceCarou extends Component {
           <div style={SauceCarou.CONTAINER_STYLE}>
             <ReactCardCarousel disable_box_shadow={true} ref={this.Carousel} autoplay={ true } autoplay_speed={ 5000 } spread="wide">
                 <div onTouchEnd={this.handleTouchEnd} onTouchStart={this.handleTouchStart} onTouchMove={this.handleTouchMove} style={ SauceCarou.CARD_STYLE }>
+                    <p className="xibeile-word">Extra @ $1 only!</p>
                     <img className="menu-img-less" src={"/saltedey-nbg.png"} alt="salted egg yolk"/>
                     <p className="xibeile-word">Salted Egg Yolk</p>
-                    <p className="xibeile-word">Extra @ $1 only!</p>
                 </div>
                 <div onTouchEnd={this.handleTouchEnd} onTouchStart={this.handleTouchStart} onTouchMove={this.handleTouchMove} style={ SauceCarou.CARD_STYLE }>
+                    <p className="xibeile-word">Extra @ $1 only!</p>
                     <img className="menu-img-less" src={"/mala-nbg.png"} alt="mala powder"/>
                     <p className="xibeile-word">Mala Powder</p>
-                    <p className="xibeile-word">Extra @ $1 only!</p>
                 </div>
                 <div onTouchEnd={this.handleTouchEnd} onTouchStart={this.handleTouchStart} onTouchMove={this.handleTouchMove} style={ SauceCarou.CARD_STYLE }>
                     <img className="menu-img-less" src={"/peanut-nbg.png"} alt="satay sauce"/>
