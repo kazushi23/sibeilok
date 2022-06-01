7c64fa9dba40bfb528e39a408b1ec1404e46800b
kazushi23 <kazushit8@gmail.com>
2022-06-01 15:11:38 +0800

    final

--- components/footer/footer.jsx
+++ components/footer/footer.jsx
@@ -2,9 +2,10 @@ export default function Footer() {
     return (
         <div className="footer-wrapper">
             <img src={"/logo100.png"} alt="loklok-logo"/>
-            <p style={{color:'white'}}>xibei shiok</p>
+            <br/>
             <br/>
             <h1 style={{color:'white', textAlign:'center'}}>© SibeiLok (2022)</h1>
+            <p style={{color:'white', textAlign:'center'}}>xi bei shiok</p>
         </div>
     )
 }
